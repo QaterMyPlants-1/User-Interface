aa15c0723cff6d9e13bd878b0daa0a990b643fe0
git-austinmccollom <austinmccollom@gmail.com>
2020-07-29 15:55:51 -0500

    created mobile nav menu and added to all pages

--- header.js
+++ header.js
@@ -3,11 +3,14 @@ document.write(`<header class="topContainer">
         <p>WATER MY</p>
         <h1>PLANTS</h1>
     </div>
+    <label class="toggle" for="toggle">&#9776;</label>
+    <input type="checkbox" id="toggle"/>
     <nav>
-        <a href='#'>HOME</a>
+        <a href='home.html'>HOME</a>
         <a href='about.html'>ABOUT</a>
         <a href='team.html'>TEAM</a>
         <a href="#">REGISTER</a>
         <a href='#'>LOG IN</a>
     </nav>
+    
 </header>`);
