d14a4c640d1a3a4d49385a1d941e6a93bf2c3509
git-austinmccollom <austinmccollom@gmail.com>
2020-07-29 11:43:16 -0500

    dynamically added header and footer to all pages using vanilla js. Currently the CSS is conflicting and causing the header to look a bit different on Austin's about and team pages

---
+++ header.js
@@ -0,0 +1,13 @@
+document.write(`<header class="topContainer">
+    <div class="title">
+        <p>WATER MY</p>
+        <h1>PLANTS</h1>
+    </div>
+    <nav>
+        <a href='#'>HOME</a>
+        <a href='about.html'>ABOUT</a>
+        <a href='team.html'>TEAM</a>
+        <a href="#">REGISTER</a>
+        <a href='#'>LOG IN</a>
+    </nav>
+</header>`);
