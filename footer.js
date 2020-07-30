7480b5f40cfcf5ac3377d42fd2fd86ac5a601090
Alecsandra Brewer <alecsandra@MacBook-Pro-2.local>
2020-07-30 09:19:32 -0700

    added alt text for images on landing and contact page, also removed the word copyright from footer to make room for mobile screens

--- footer.js
+++ footer.js
@@ -1,5 +1,5 @@
 document.write(`<footer>
-    <p>Copyright 2020 Water My Plants</p>
+    <p>2020 Water My Plants</p>
     <a href="#" class="fa fa-facebook"></a>
     <a href="#" class="fa fa-twitter"></a>
     <a href="#" class="fa fa-instagram"></a>
